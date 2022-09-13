fe544c32b73b605642c3ee1cfc2e7e3d020d5c74
PaPasha00 <vasev-2020@bk.ru>
2022-09-13 21:49:33 +0300

    Update

--- Script.js
+++ Script.js
@@ -1,5 +1,12 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
+const s = document.getElementById("watch");
+let score = 0;
+
+setInterval(() => {
+    score += 1;
+    s.textContent = score;
+}, 200);
 
 document.addEventListener("keydown", function(event) {
     jump();
@@ -11,14 +18,16 @@ function jump() {
     }
     setTimeout(function () {
         dino.classList.remove("jump")
-    }, 300)
+    }, 500)
 }
 
 let isAlive = setInterval ( function () {
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
 
-    if (cactusLeft < 53 && cactusLeft > 20 && dinoTop >= 200) {
+    if (cactusLeft < 60 && cactusLeft > 20 && dinoTop >= 170) {
         alert("!!!Game Over!!!")
+        score = -1
     }
 }, 10)
+
